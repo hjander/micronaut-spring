2204ab7762896534f134705f4d393b933cc16ee0
Hendrik Jander <hendrik.jander@isato.cloud>
2018-12-21 02:06:48 +0100

    cleanup

--- examples/greeting-service/src/main/java/aws/lambda/custom/runtime/micronaut/MicronautAwsLambdaCustomRuntimeClient.java
+++ examples/greeting-service/src/main/java/aws/lambda/custom/runtime/micronaut/MicronautAwsLambdaCustomRuntimeClient.java
@@ -2,8 +2,7 @@ package aws.lambda.custom.runtime.micronaut;
 
 
 import aws.lambda.custom.runtime.AwsLambdaCustomRuntimeClient;
-import aws.lambda.custom.runtime.model.InitialisationError;
-import aws.lambda.custom.runtime.model.InvocationError;
+import aws.lambda.custom.runtime.model.LambdaError;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
@@ -12,12 +11,8 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.Header;
 import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
-import io.micronaut.jackson.annotation.JacksonFeatures;
 
 import javax.validation.constraints.NotBlank;
-
-import static com.fasterxml.jackson.databind.DeserializationFeature.UNWRAP_ROOT_VALUE;
-import static com.fasterxml.jackson.databind.SerializationFeature.WRAP_ROOT_VALUE;
 
 @Client(value = "${aws.lambda.runtime.api}", path = AwsLambdaCustomRuntimeClient.RUNTIME_PATH)
 public interface MicronautAwsLambdaCustomRuntimeClient extends AwsLambdaCustomRuntimeClient {
@@ -31,23 +26,13 @@ public interface MicronautAwsLambdaCustomRuntimeClient extends AwsLambdaCustomRu
     @Post(value = "/invocation/{awsRequestId}/response", produces = MediaType.APPLICATION_JSON)
     HttpResponse postFunctionInvocationResponse(String awsRequestId, @Body APIGatewayProxyResponseEvent apgResponse);
 
-    /*
 
-    function toLambdaErr({ name, message, stack }) {
-      return {
-        errorType: name,
-        errorMessage: message,
-        stackTrace: (stack || '').split('\n').slice(1),
-      }
-    }
-
-         */
     @Post(value = "/invocation/{awsRequestId}/error", consumes = MediaType.APPLICATION_JSON)
     HttpResponse<Void> postFunctionInvocationError(@Header("Lambda-Runtime-Function-Error-Type") String errorType,
-                                                   @NotBlank String awsRequestId, @Body InvocationError error);
+                                                   @NotBlank String awsRequestId, @Body LambdaError error);
 
 
     @Post(value = "/init/error", produces = MediaType.APPLICATION_JSON)
-    void postRuntimeInitError(InitialisationError initError);
+    void postRuntimeInitError(LambdaError initError);
 
 }

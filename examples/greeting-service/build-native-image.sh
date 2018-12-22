7856c7f0ca39bcfab56ffc98dfee6af0f0809b95
Hendrik Jander <hendrik.jander@isato.cloud>
2018-12-22 01:31:44 +0100

    add --allow-incomplete-classpath flag

--- examples/greeting-service/build-native-image.sh
+++ examples/greeting-service/build-native-image.sh
@@ -1,14 +1,16 @@
-./gradlew assemble
+./gradlew clean build assemble
 java -cp build/libs/greeting-service-all.jar io.micronaut.graal.reflect.GraalClassLoadingAnalyzer
-native-image --no-server \
+native-image --verbose \
+             --no-server \
              --class-path build/libs/greeting-service-all.jar \
+             --allow-incomplete-classpath \
              -H:ReflectionConfigurationFiles=build/reflect.json \
              -H:EnableURLProtocols=http \
              -H:IncludeResources="logback.xml|application.yml|META-INF/services/*.*" \
              -H:Name=greeting-service \
-             -H:Class=greeting.example.Application \
+             -H:Class=aws.lambda.custom.runtime.micronaut.Bootstrap \
              -H:+ReportUnsupportedElementsAtRuntime \
-             -H:+AllowVMInspection \
+             -H:-AllowVMInspection \
              -H:-UseServiceLoaderFeature \
              --rerun-class-initialization-at-runtime='sun.security.jca.JCAUtil$CachedSecureRandomHolder,javax.net.ssl.SSLContext' \
              --delay-class-initialization-to-runtime=io.netty.handler.codec.http.HttpObjectEncoder,io.netty.handler.codec.http.websocketx.WebSocket00FrameEncoder,io.netty.handler.ssl.util.ThreadLocalInsecureRandom,com.sun.jndi.dns.DnsClient
